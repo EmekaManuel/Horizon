ab73bcb10adad4a39724ab676ffae7063366fab3
Onyewuchi Emeka <marrnuel123@.com>
2024-05-05 09:57:28 -0500

    chore(): completed the header section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,28 @@
+import HeaderBox from "@/components/headerBox";
+import TotalBalanceBox from "@/components/totalBalanceBox";
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const loggedIn = { firstName: "Manuel", lastName: "Emeka" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn.firstName || "Guest"}
+            subtext="Access and Manage Your Accounts and Transactions"
+          />
+          <TotalBalanceBox
+            accounts={1}
+            totalBanks={1}
+            totalCurrentBalance={1250.78}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
